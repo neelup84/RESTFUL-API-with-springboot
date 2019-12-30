1a256a9394bf268d65f2b4991190d39a040e382f
Neelima Patnaik <51104846+neelup84@users.noreply.github.com>
2019-12-29 18:41:31 -0600

    Add files via upload
    tight coupling using binary search algorithm

--- BinarySearchimpl.java
+++ BinarySearchimpl.java
@@ -1,9 +1,17 @@
 package com.neelima.springin5steps;
 
 public class BinarySearchimpl {
-	
-	public int binarySearch(int [] numberes, int numbersToSearchFor) {
+	 	
+	public int binarySearch(int [] numbers, int numbersToSearchFor) {
+		
+		BubbleSortAlgorithm bubbleSortAlgorithm = new  BubbleSortAlgorithm();
+		int [] sortedNumbers = bubbleSortAlgorithm.sort(numbers);
 
+    // implementing sorting logic
+		
+    //Bubble sort Algorithm
+	
+		
 		return 3;
 		
 		
---
+++ BubbleSortAlgorithm.java
@@ -0,0 +1,11 @@
+package com.neelima.springin5steps;
+
+public class BubbleSortAlgorithm {
+
+	public int[] sort (int[] numbers) {
+		
+		// Logic for bubble sort
+		
+		return numbers;
+	}
+}
